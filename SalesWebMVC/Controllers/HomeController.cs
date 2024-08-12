9b0c8d6f8c8d796a9764e8716595667a7a64c39e
Washington Junior <washingtonluiz.bfjr@gmail.com>
2024-08-12 10:58:54 -0300

    First controller and Razon pages tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["email"] = "washingtonluiz.bfjr@gmail.com";
+            ViewData["Student"] = "Washington Junior";
 
             return View();
         }
