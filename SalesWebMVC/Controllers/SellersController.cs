e2e615d2dd62ca428310357e95f6ea44e144e79e
Washington Junior <washingtonluiz.bfjr@gmail.com>
2024-08-13 16:16:01 -0300

    Delete seller

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -2,6 +2,8 @@
 using SalesWebMVC.Services;
 using SalesWebMVC.Models;
 using SalesWebMVC.Models.ViewModels;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
 
 namespace SalesWebMVC.Controllers
 {
@@ -34,6 +36,31 @@ namespace SalesWebMVC.Controllers
 		public IActionResult Create(Seller seller)
 		{
 			_sellerService.Insert(seller);
+			return RedirectToAction(nameof(Index));
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var obj = _sellerService.FindById(id.Value);
+			
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			return View(obj);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Delete(int id)
+		{
+			_sellerService.Remove(id);
 			return RedirectToAction(nameof(Index));
 		}
 	}
